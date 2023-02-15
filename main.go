344137c89c16807264796e387a1e982dc368b6ad
Laurence <laurence.jones@live.co.uk>
2023-02-15 13:20:42 +0000

    Write to stdout instead of hardcoded file

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"path"
 
 	"github.com/crowdsecurity/crowdsec/pkg/cticlient"
 )
@@ -22,24 +21,9 @@ func main() {
 		Limit: intPtr(1000),
 	})
 
-	outputDir := os.Getenv("OUTPUT_DIR")
-	if outputDir == "" {
-		outputDir = "./"
-	}
-
-	filePath := path.Join(outputDir, "fire.txt")
-	file, err := os.Create(filePath)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
 	for {
 		items, err := paginator.Next()
 		if err != nil {
-			if err == cticlient.ErrUnauthorized {
-				os.Remove(filePath)
-			}
 			log.Fatalf("Error whilst fetching CTI data got %s", err.Error())
 		}
 		if items == nil {
@@ -47,7 +31,7 @@ func main() {
 		}
 
 		for _, item := range items {
-			file.WriteString(item.Ip + "\n")
+			os.Stdout.WriteString(item.Ip + "\n")
 		}
 	}
 }
